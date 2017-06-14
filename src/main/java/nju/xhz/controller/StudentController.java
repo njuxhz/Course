04d569756723f29d97e3759d21d2033f7c68041e
njuxhz <qyeung@qq.com>
2017-06-14 21:04:54 +0800

    Version 7

--- src/main/java/nju/xhz/controller/StudentController.java
+++ src/main/java/nju/xhz/controller/StudentController.java
@@ -3,12 +3,11 @@ package nju.xhz.controller;
 import nju.xhz.model.Student;
 import nju.xhz.service.IStudentService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@Controller
+@RestController
 public class StudentController {
     @Autowired
     private IStudentService studentService;
